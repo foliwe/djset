281f924670e3c8d3282888d187b4f3ee09215a52
aqdas95 <aqdasmalikcs@gmail.com>
2018-09-22 22:26:01 +0500

    PDF

--- app/mailers/ticket_mailer.rb
+++ app/mailers/ticket_mailer.rb
@@ -1,7 +1,10 @@
 class TicketMailer < ApplicationMailer
-  default from: "from@example.com"
+  default from: "djtakeawayint@gmail.com"
 
   def email_ticket
+    attachments["ticket.pdf"] = WickedPdf.new.pdf_from_string(
+      render_to_string(pdf: 'ticket', template: 'home/ticket.html.erb')
+    )
     mail(to: "aqdasmalik8@gmail.com", subject: 'Sample Email')
   end
 end
